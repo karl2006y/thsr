5021a19c077ef0f2c0f6d450709e326e2396aa45
karl <karl2015y@gmail.com>
2019-08-16 23:11:49 +0800

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./plugins/element.js";
+import "bootstrap/scss/bootstrap.scss";
+import "bootstrap";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
