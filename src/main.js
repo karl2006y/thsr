25106dbca81e09658c714646c9c7dbe8f3af98e7
karl <karl2015y@gmail.com>
2019-08-17 00:03:54 +0800

    import Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./plugins/element.js";
-import "bootstrap/scss/bootstrap.scss";
-import "bootstrap";
+import Vue from 'vue'
+import './plugins/fontawesome'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ src/views/Home/index.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<template src="./template.html"></template>
+<style lang="css" src="./style.css" scoped></style>
